6e3b48c6875b51bb2c70a5dbf9cfd083b2c3d061
dionisk <33986714+dionisk@users.noreply.github.com>
2021-04-21 09:26:11 +0300

    Adding ApiKeyMiddleWare

---
+++ LogImporter/LogImporter/Filters/ApiKeyMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace SecuringWebApiUsingApiKey.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        readonly string[] ApiKeys = { "ApiKey1", "ApiKey2" };
+        readonly string extractedApiKey;
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            bool IsValidKeyName = false;
+            IConfiguration appSettings;
+            string apiKey = null;           
+            foreach (string key in ApiKeys)
+            {
+                if (context.Request.Headers.TryGetValue(key, out var extractedApiKey))
+                {
+                    IsValidKeyName = true;
+                    appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+                    apiKey = appSettings.GetValue<string>(key);
+                    break;
+                }                
+            }
+
+            if (!IsValidKeyName)
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided. (Using ApiKeyMiddleware) ");
+                return;
+            }       
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client. (Using ApiKeyMiddleware)");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ LogImporter/LogImporter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LogImporter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
